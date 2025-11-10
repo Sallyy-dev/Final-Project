cd648f4a160cffa8393522c2202b01d08e1f2447
sally gamal <salyjmalslam@gmail.com>
2025-11-10 17:26:09 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const connectDB = require("./config/db");
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
